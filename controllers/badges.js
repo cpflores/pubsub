f5ae950e437c7164ffc528408472d3c2c106f628
Chadwick Flores <chadwickflores@gmail.com>
2014-12-13 11:40:07 -0600

    pseudo code update

--- controllers/badges.js
+++ controllers/badges.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var _ = requre('underscore');
+
 /**
 * Send badges to model to be saved
 */
 exports.save = function(req, res, next) {
-	var badges = req.body;	
+	var badges = _.clone(req.body);	
 	model.save(badges, fucntion(err){
 		if (err) return res.json(503, { error: true });
 		next();
