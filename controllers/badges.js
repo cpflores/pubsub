d91b3f53227b2b4fe41f6a03ffddf776b1031369
Chadwick Flores <chadwickflores@gmail.com>
2014-12-13 11:37:50 -0600

    pseudo code

--- controllers/badges.js
+++ controllers/badges.js
@@ -1,10 +1,17 @@
 'use strict';
 
 /**
-* Send badges to model
+* Send badges to model to be saved
 */
+exports.save = function(req, res, next) {
+	var badges = req.body;	
+	model.save(badges, fucntion(err){
+		if (err) return res.json(503, { error: true });
+		next();
+	});
+};
 
-
-exports.save = function() {};
-
+/**
+*send badges to pub/sub socket in model
+*/
 exports.send = function() {};
