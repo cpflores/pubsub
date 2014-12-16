9e934b0cbc50aa098efa66dc568920d190dac569
Chadwick Flores <chadwickflores@gmail.com>
2014-12-15 21:58:07 -0600

    add redis.js

--- models/badges.js
+++ models/badges.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var redis = require('../lib/redis');
+
 /**
 * Save badges to database
-*
-*
+* @param {Array} badges
+* @param {Function} callback
 */
 
 exports.save = function(badges, callback) {
