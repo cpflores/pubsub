accd88fc3c52b040e73faa79c998adeb29190c89
Chadwick Flores <chadwickflores@gmail.com>
2014-12-18 19:33:43 -0600

    connect controller

--- app.js
+++ app.js
@@ -2,5 +2,10 @@
 
 var express = require('express');
 var app = express();
+var badges = require('./controllers/badges');
 
-app.use();
+app.use(express.json());
+
+app.post('/', badges.save, badges.send);
+
+app.listen(8000);
